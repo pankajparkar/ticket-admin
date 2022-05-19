127fe07a58b34f16cc0cdcce2bad75f579234f79
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-05-19 19:07:53 +0800

    Added both the charts in action.

--- src/app/components/charts/number-chart/number-chart.component.ts
+++ src/app/components/charts/number-chart/number-chart.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { single } from './data';
 
 @Component({
   selector: 'bmc-number-chart',
   templateUrl: './number-chart.component.html',
   styleUrls: ['./number-chart.component.sass']
 })
-export class NumberChartComponent implements OnInit {
+export class NumberChartComponent {
 
-  constructor() { }
+  single!: any[];
+  view: [number, number] = [700, 400];
 
-  ngOnInit(): void {
+  colorScheme: any = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+  cardColor: string = '#232837';
+
+  constructor() {
+    Object.assign(this, { single });
   }
-
 }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,43 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { single } from './data';
-import { Color } from '@swimlane/ngx-charts'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'bmc-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.sass']
 })
-export class DashboardComponent implements OnInit {
-
-  single!: any[];
-  multi!: any[];
-
-  view: [number, number] = [700, 400];
-
-  // options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Country';
-  showYAxisLabel = true;
-  yAxisLabel = 'Population';
-  colorScheme: any = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  };
-  customColors = [
-    {
-      name: 'test',
-      value: '#ff0000'
-    }
-  ];
-
-  constructor() {
-    Object.assign(this, { single })
-  }
-
-  ngOnInit(): void {
-  }
+export class DashboardComponent {
 
 }
